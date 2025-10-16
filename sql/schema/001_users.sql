9f8211e965c33fb344d3d53bddf0099f56f591df
groovypotato <gleasoncr@gmail.com>
2025-10-16 16:34:25 -0500

    CH5 L3 STEP3

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    email TEXT UNIQUE NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
