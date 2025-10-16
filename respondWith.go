918c755b676c4d0fc62b0ba2eb3ff4816761d883
groovypotato <gleasoncr@gmail.com>
2025-10-16 13:35:19 -0500

    CH4 L6

---
+++ respondWith.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type returnError struct {
+		Error string `json:"error"`
+	}
+	errorMsg := returnError {
+			Error: msg,
+		}
+		dat, err := json.Marshal(errorMsg)
+		if err != nil {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(code)
+			fmt.Fprintf(w, "error: %s\n", msg)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write(dat)
+}
+
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	validMsg := payload
+		dat, err := json.Marshal(validMsg)
+		if err != nil {
+			respondWithError(w,400,"Something went wrong")
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write(dat)
+}
--- validatechirphandler.go
+++ validatechirphandler.go
@@ -2,29 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 func validatechirpHandler (w http.ResponseWriter, r *http.Request) {
-	type returnError struct {
-		Error string `json:"error"`
-	}
 	if r.Method != "POST"{
-		errorMsg := returnError {
-			Error: "method not allowed",
-		}
-		dat, err := json.Marshal(errorMsg)
-		if err != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(405)
-			fmt.Fprintf(w, "error: method nto allowed\n")
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(405)
-		w.Write(dat)
-		return
+		respondWithError(w,405,"method not allowed")
 	}
 	type parameters struct {
 		Body string `json:"body"`
@@ -37,50 +20,14 @@ func validatechirpHandler (w http.ResponseWriter, r *http.Request) {
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		errorMsg := returnError {
-			Error: "Something went wrong",
-		}
-		dat, errs := json.Marshal(errorMsg)
-		if errs != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "error: incorrect format\n")
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(dat)
+		respondWithError(w,400,"Something went wrong")
 		return
 	}
 	if len(params.Body) > 140 {
-		errorMsg := returnError {
-			Error: "Chirp is too long",
-		}
-		dat, errs := json.Marshal(errorMsg)
-		if errs != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "error: Chirp is too long\n")
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(dat)
+		respondWithError(w,400,"Chirp is too long")
 		return
 	}else {
-		validMsg := returnValid {
-			Valid: true,
-		}
-		dat, err := json.Marshal(validMsg)
-		if err != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "error: something went wrong\n")
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		w.Write(dat)
+		respondWithJSON(w,200, returnValid{Valid: true})
 		return
 	}
 }
