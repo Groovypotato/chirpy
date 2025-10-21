c304c06c515ea86944c9dd4a9268bd6e99e7ff99
groovypotato <gleasoncr@gmail.com>
2025-10-21 18:17:50 -0500

    CH6 L2

---
+++ internal/auth/passwordHalder.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"runtime"
+
+	"github.com/alexedwards/argon2id"
+)
+
+func HashPassword(password string) (string, error) {
+
+	params := &argon2id.Params{
+		Memory:      128 * 1024,
+		Iterations:  4,
+		Parallelism: uint8(runtime.NumCPU()),
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+	hash, err := argon2id.CreateHash(password, params)
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
+}
+
+func CheckPasswordHash(password, hash string) (bool, error)
